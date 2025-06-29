43b56c55de21553f1d9a96f5fddb912237bfd135
johani23 <ahmad.a.johani@gmail.com>
2025-06-29 11:43:46 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Heartbridge UI</h1>
+      <p>واجهة التقييم قيد التجهيز ✨</p>
+    </div>
+  );
+}
+
+export default App;
