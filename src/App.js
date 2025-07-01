b0591f6c06a10f1b7521eb1fe14fbbf3c84abf7a
johani23 <ahmad.a.johani@gmail.com>
2025-07-01 11:39:12 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Landing from './pages/Landing';
-import Quiz from './pages/Quiz';
-import Analyzer from './pages/Analyzer';
+import Landing from './components/Landing';
+import Quiz from './components/Quiz';
+import Analyzer from './components/Analyzer';
 import './App.css';
 
 function App() {
